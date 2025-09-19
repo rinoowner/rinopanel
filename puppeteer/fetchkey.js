6834070152af7589e9e0e64462025f749bde6f53
Sukhveer Singh <bid70840@gmail.com>
2025-09-19 05:52:51 +0530

    Update fetchkey.js

--- puppeteer/fetchkey.js
+++ puppeteer/fetchkey.js
@@ -52,7 +52,9 @@ export async function fetchKey(loaderId, durationId) {
   await page.waitForTimeout(3000);
 
   await page.goto('https://tenda-mod.ggff.net/dashboard.php', { waitUntil: 'networkidle2' });
-  await page.screenshot({ path: 'login-debug.png' });
+ await page.screenshot({ path: 'login-debug.png' });
+ console.log("📸 Screenshot taken: login-debug.png");
+
   await page.waitForTimeout(1000);
 
   const isLoggedIn = await page.evaluate(() => {
@@ -85,5 +87,6 @@ export async function fetchKey(loaderId, durationId) {
   await browser.close();
   return finalOutput;
 }
+
 
 
